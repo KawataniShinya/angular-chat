7fabcb0a4a75588540102c45036e5e822c7aa157
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-09 18:53:45 +0900

    ready to use firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAqIpAXwyBMdKITv-pbP6v51RgG2XRM7WQ",
+    authDomain: "angular-chat-3b3e5.firebaseapp.com",
+    projectId: "angular-chat-3b3e5",
+    storageBucket: "angular-chat-3b3e5.appspot.com",
+    messagingSenderId: "541432985507",
+    appId: "1:541432985507:web:3ea44bba502c47575f0c16"
+  }
 };
 
 /*
