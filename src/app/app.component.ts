c0836d179fe19a9c3946c94087f35fa6dfb2abba
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-10 16:58:40 +0900

    add feature : push comment to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
 import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 const CURRENT_USER: User = new User(1, '五十川 洋平');
 const ANOTHER_USER: User = new User(2, '竹井 賢治');
@@ -22,18 +22,23 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
 
-  comments = COMMENTS;
+  // comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = '';
   item$: Observable<any>;
 
   constructor(private db: AngularFireDatabase){
     this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.valueChanges();
   }
 
   addComment(comment: string): void {
     if (comment) {
-      this.comments.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(new Comment(this.currentUser, comment));
+      this.comment = '';
     }
   }
 
