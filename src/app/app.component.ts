dfacdcf34cdeca582084807dddd1138d9d92de66
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-12 22:04:15 +0900

    add routing (404)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ac-root',
   template: `
-    <ac-header></ac-header>
-    <ac-chat></ac-chat>
+    <ac-header></ac-header>cd
+    <div class="page">
+      <router-outlet></router-outlet>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
