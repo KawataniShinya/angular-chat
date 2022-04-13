ab4c00dc2247d11a414b7c109c57561d31fbe8c7
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-14 00:02:42 +0900

    deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAqIpAXwyBMdKITv-pbP6v51RgG2XRM7WQ",
+    authDomain: "angular-chat-3b3e5.firebaseapp.com",
+    projectId: "angular-chat-3b3e5",
+    storageBucket: "angular-chat-3b3e5.appspot.com",
+    messagingSenderId: "541432985507",
+    appId: "1:541432985507:web:3ea44bba502c47575f0c16"
+  }
 };
